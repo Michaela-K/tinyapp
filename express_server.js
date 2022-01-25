a6e87929f28ce6364029d45aef29dfd6e0ee06a7
Michaela-K <michaelakodes@gmail.com>
2022-01-25 01:34:04 +0000

    implemented random string

--- express_server.js
+++ express_server.js
@@ -2,20 +2,73 @@ const express = require("express");
 const app = express();
 const PORT = 8000; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {   //JSON string representing the entire urlDatabase object
-  res.json(urlDatabase);
-}); 
+app.get("/urls", (req, res) => {
+  //JSON string representing the entire urlDatabase object
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
+app.get("/urls.json", (req, res) => {
+  //JSON string representing the entire urlDatabase object
+  res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL,
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+});
+
+function generateRandomString(length) {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+generateRandomString(7);
